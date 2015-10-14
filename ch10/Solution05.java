7c3d809142bab4f5e5e1a8fb46e7f5997f536065
Johnsonys <yuansheng226@gmail.com>
2015-10-14 15:30:14 -0400

    Update Solution05.java

--- ch10/Solution05.java
+++ ch10/Solution05.java
@@ -9,6 +9,7 @@ public class Solution05
 	 *  
 	**/
 
+	//for loop version
 	public int search(String[] str, String target)
 	{
 		for(int i = 0; i < str.length; i++)
@@ -18,5 +19,45 @@ public class Solution05
 		}
 		return -1;
 	}
+	
+	//binary search version
+      	public static int search(String[] strings, String str, int first, int last) {
+    	  if (first > last) 
+    		  return -1;
+    	  int mid = (first + last) / 2;
+    	  //if mid is empty, find the closest non-empty string
+    	  if (strings[mid].isEmpty()) {
+    		  int left = mid - 1;
+    		  int right = mid + 1;
+    		  while (true) {
+    			  if (left < first && right > last) {
+    				  return -1;
+    			  } else if (right <= last && !strings[right].isEmpty()) {
+    				  mid = right;
+    				  break;
+    			  } else if (left >= first && !strings[left].isEmpty()) {
+    				  mid = left;
+    				  break;
+    			  }
+    			  right++;
+    			  left--;
+    		  }
+    	  }
+    	  //check for string, and recurse.
+    	  if (str.equals(strings[mid])) {
+    		  return mid;           //found
+    	  } else if (strings[mid].compareTo(str) < 0) {   //search the right side
+    		  return search(strings, str, mid + 1, last);
+    	  } else {
+    		  return search(strings, str, first, mid - 1); //search the left side
+    	  }
+	}
+	public static int search(String[] strings, String str) {
+	      if (strings == null || str == null || str == "") {
+	    	  return -1;
+	      }
+	  return search(strings, str, 0, strings.length - 1);  
+	}
+
 }
 
